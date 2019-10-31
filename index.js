8875bcde6b5f6d92ddc7d18a4aac1adafd60214a
Fletcher Goss <44245838+IAmFletcher@users.noreply.github.com>
2019-10-31 11:42:52 +0000

    Refactor Guild
    Add a debug tool that allows the owner to initiate the MESSAGE_UPDATE process.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const process = require('process');
 
-const Guild = require('./src/Guild');
+const Guild = require('./lib/Guild');
 
 const client = require('./src/client');
 const gateway = require('./src/gateway');
@@ -23,23 +23,45 @@ gateway.on('GUILD_CREATE', (msg) => {
     return;
   }
 
-  guilds[msg.d.id] = new Guild({
-    id: msg.d.id,
-    ownerID: msg.d.owner_id,
-    roles: msg.d.roles
-  });
+  guilds[msg.d.id] = new Guild(msg.d);
+});
+
+gateway.on('GUILD_UPDATE', (msg) => {
+  guilds[msg.d.id].update(msg.d);
+});
+
+gateway.on('GUILD_DELETE', (msg) => {
+  if (msg.d.unavailable == null) {
+    delete guilds[msg.d.id];
+  }
+});
+
+gateway.on('GUILD_ROLE_CREATE', (msg) => {
+  guilds[msg.d.guild_id].roleCreate(msg.d.role);
+});
+
+gateway.on('GUILD_ROLE_UPDATE', (msg) => {
+  guilds[msg.d.guild_id].roleUpdate(msg.d.role);
+});
+
+gateway.on('GUILD_ROLE_DELETE', (msg) => {
+  guilds[msg.d.guild_id].roleDelete(msg.d);
 });
 
 gateway.on('MESSAGE_CREATE', (msg) => {
-  guilds[msg.d.guild_id].messageCreate(msg);
+  guilds[msg.d.guild_id].messageCreate(msg.d);
 });
 
 gateway.on('MESSAGE_UPDATE', (msg) => {
-  guilds[msg.d.guild_id].messageUpdate(msg);
+  guilds[msg.d.guild_id].messageUpdate(msg.d);
 });
 
 gateway.on('MESSAGE_DELETE', (msg) => {
-  guilds[msg.d.guild_id].messageDelete(msg);
+  guilds[msg.d.guild_id].messageDelete(msg.d);
+});
+
+gateway.on('MESSAGE_DELETE_BULK', (msg) => {
+  guilds[msg.d.guild_id].messageDeleteBulk(msg.d);
 });
 
 gateway.on('MESSAGE_REACTION_ADD', (msg) => {
@@ -47,7 +69,7 @@ gateway.on('MESSAGE_REACTION_ADD', (msg) => {
     return;
   }
 
-  guilds[msg.d.guild_id].reactionAdd(msg);
+  guilds[msg.d.guild_id].messageReactionAdd(msg.d);
 });
 
 gateway.on('MESSAGE_REACTION_REMOVE', (msg) => {
@@ -55,23 +77,7 @@ gateway.on('MESSAGE_REACTION_REMOVE', (msg) => {
     return;
   }
 
-  guilds[msg.d.guild_id].reactionRemove(msg);
-});
-
-gateway.on('MESSAGE_DELETE_BULK', (msg) => {
-  guilds[msg.d.guild_id].messageDeleteBulk(msg);
-});
-
-gateway.on('GUILD_ROLE_CREATE', (msg) => {
-  guilds[msg.d.guild_id].addRole(msg.d.role.id, msg.d.role.name);
-});
-
-gateway.on('GUILD_ROLE_UPDATE', (msg) => {
-  guilds[msg.d.guild_id].updateRoleName(msg.d.role.id, msg.d.role.name);
-});
-
-gateway.on('GUILD_ROLE_DELETE', (msg) => {
-  guilds[msg.d.guild_id].deleteRole(msg.d.role_id);
+  guilds[msg.d.guild_id].messageReactionRemove(msg.d);
 });
 
 process.on('SIGINT', () => {
